fe347c566d4d5a0c679306b266bb0094b40e34c5
richfjiq <rfjiq1986@gmail.com>
2023-06-29 21:15:08 -0600

    chore: add cars.service

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,17 +1,18 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-  private cars = ['Toyota', 'Honda', 'Jeep'];
+  constructor(private readonly carsService: CarsService) {}
 
   @Get()
   getAllCars() {
-    return this.cars;
+    return this.carsService.findAll();
   }
 
   @Get(':id')
   getCarById(@Param('id') id: string) {
     console.log({ id });
-    return this.cars[id];
+    return this.carsService.findOneById(+id);
   }
 }
