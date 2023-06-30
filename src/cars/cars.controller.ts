71b7463e970103636453675cf0d3382fdb381477
richfjiq <rfjiq1986@gmail.com>
2023-06-29 20:05:48 -0600

    set up api

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('cars')
+export class CarsController {
+  private cars = ['Toyota', 'Honda', 'Jeep'];
+
+  @Get()
+  getAllCars() {
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarById(@Param('id') id: string) {
+    console.log({ id });
+    return this.cars[id];
+  }
+}
